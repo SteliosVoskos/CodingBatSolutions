2cf34b747c9e175a94bafc12b1a3643e8db01725
Stelios Voskos <s.voskos@googlemail.com>
2017-08-29 08:53:40 +0300

    fornt3 | removed redundant conditions

--- test/front3.spec.js
+++ test/front3.spec.js
@@ -2,17 +2,11 @@ var assert = require('assert');
 var expect = require('chai').expect;
 
 function front3(str) {
-    if (typeof str !== 'string') {
+    if (typeof str !== 'string' || str.length === 0) {
         return '';
     }
-    if (str.length === 0) {
-        return '';
-    }
-    if (str.length === 1) {
+    if (str.length === 2 || str.length === 1) {
         return str+str+str;
-    }
-    if (str.length === 2) {
-        return str.substring(0,2)+str.substring(0,2)+str;
     }
     return str.substring(0,3)+str.substring(0,3)+str;
 }
